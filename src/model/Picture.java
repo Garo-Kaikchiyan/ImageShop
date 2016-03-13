020136dd502e98b6e9f8382c868b5c3f3aade954
Garo-Kaikchiyan <delta.lead@yahoo.com>
2016-03-13 13:22:36 +0200

    FirstCommit

---
+++ src/model/Picture.java
@@ -0,0 +1,34 @@
+package model;
+import java.util.ArrayList;
+
+public class Picture {
+	
+	private String url;
+	private static int ID = 0;
+	private int id;
+	private double price;
+	private ArrayList<String> categories;
+	private int timesBought;
+	
+	Picture(ArrayList<String> categories, String url, double price){
+		this.categories = categories;
+		this.url = url;
+		this.price = price;
+		id = ID++;
+	}
+	public ArrayList<String> getCategories() {
+		return categories;
+	}
+	@Override
+	public String toString() {
+		return "-----Picture " + id + "-----";
+	}
+	public void showInfo(){
+		System.out.println("-----Picture " + id + "-----");
+		for(String category: categories)
+			System.out.print(category + " ");
+		System.out.println("Price : " + price);
+		System.out.println("Times bought : " + timesBought);
+		
+	}
+}
